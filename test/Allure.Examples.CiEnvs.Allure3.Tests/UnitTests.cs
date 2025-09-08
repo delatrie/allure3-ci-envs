bc4ae76aecfa88728c183f35bfa5206881574933
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-09-08 22:55:26 +0700

    Initialize the project

---
+++ test/Allure.Examples.CiEnvs.Allure3.Tests/UnitTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Allure.Examples.CiEnvs.Allure3.Tests;
+
+class UnitTests
+{
+    private Random rnd;
+
+    [SetUp]
+    public void Init()
+    {
+        this.rnd = new Random();
+    }
+
+    [Test]
+    public void AlwaysPasses()
+    {
+    }
+
+    [Test]
+    public void FailsWith25Percent()
+    {
+        Assert.That(this.rnd.NextInt64(0, 4), Is.Not.Zero);
+    }
+
+    [Test]
+    public void BreaksWith25Percent()
+    {
+        if (this.rnd.NextInt64(0, 4) == 0)
+        {
+            throw new Exception("An artifitial exception");
+        }
+    }
+
+    [Test]
+    public void FailsBreaksOrSkipesWith30Percent()
+    {
+        switch (this.rnd.NextInt64(0, 10))
+        {
+            case 0:
+                throw new Exception("An artifitial exception");
+            case 1:
+                Assert.Fail("An artificial failure");
+                return;
+            case 2:
+                Assert.Ignore();
+                return;
+        }
+    }
+}
